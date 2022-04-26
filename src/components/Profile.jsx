5b82c88f691c12848ee1f200272c9c99945eac9a
Prakhar Mishra <mprakhar35@gmail.com>
2022-04-26 19:08:57 +0000

    2nd commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <h1>Profile Page</h1>
+
+        <p>Name:</p>
+        <p>Age:</p>
+        <p>Email:</p>
+        
+    </div>
+  )
+}
+
+export default Profile
